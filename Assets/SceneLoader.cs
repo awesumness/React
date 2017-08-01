77e8b696e49d5b9767244eaa6101464ecddd549f
Jeff <awesumness@gmail.com>
2017-07-31 22:42:08 -0700

    Force vertical screen orientation. Main menu with Exit and Play button. Exit button closes app. Play button opens blank level. Android back button in level will return to main menu. Back button in main menu will close app.

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+  // Use this for initialization
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+
+  public void loadLevel1() {
+    SceneManager.LoadScene("Level1");
+  }
+
+  public void loadMainMenu() {
+    SceneManager.LoadScene("Main Menu");
+  }
+
+
+  public void quit() {
+    Application.Quit();
+  }
+}
